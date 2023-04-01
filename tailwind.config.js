39019114c68bcc4d9e0e42b120a0e7fd6c3f26c7
NuntapongS <nuntapong@odds.team>
2023-04-01 18:27:19 +0700

    config color title

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        Text: {
+          title: "#E53A68",
+        },
+      },
+    },
+    plugins: [],
   },
-  plugins: [],
 };

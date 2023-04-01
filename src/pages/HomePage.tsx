3fb3b501326937f3a16be3e9328a9b628af3a9dd
NuntapongS <nuntapong@odds.team>
2023-04-01 19:05:27 +0700

    edit home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,16 @@
 const HomePage = () => {
-  return <>Hello world</>;
+  return (
+    <div className="h-screen w-full bg-[url('https://i.ytimg.com/vi/9sNQFJAb3Ss/maxresdefault.jpg')] bg-cover">
+      <div className="flex flex-col ">
+        <div className="flex flex-col justify-center items-center w-full h-full text-3xl text-Text-title tracking-[6px] font-semibold pt-[250px] pb-10">
+          Nuntapong (Pat) Siripanyawong |
+        </div>
+        <div className="flex justify-center w-full text-xl text-Text-title tracking-[6px] font-semibold">
+          Welcome To My Page
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;

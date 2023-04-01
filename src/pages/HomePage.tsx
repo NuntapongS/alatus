32bb4bef1b3f5e615b9450889c44fd7f0f993418
NuntapongS <nuntapong@odds.team>
2023-04-01 19:27:57 +0700

    add about me button

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,12 +2,17 @@ const HomePage = () => {
   return (
     <div className="h-screen w-full bg-[url('https://i.ytimg.com/vi/9sNQFJAb3Ss/maxresdefault.jpg')] bg-cover">
       <div className="flex flex-col ">
-        <div className="flex flex-col justify-center items-center w-full h-full text-3xl text-Text-title tracking-[6px] font-semibold pt-[250px] pb-10">
+        <div className="flex flex-col justify-center items-center text-3xl text-Text-title tracking-[6px] font-semibold pt-[250px] pb-10">
           Nuntapong (Pat) Siripanyawong |
         </div>
-        <div className="flex justify-center w-full text-xl text-Text-title tracking-[6px] font-semibold">
+        <div className="flex justify-center text-xl text-Text-title tracking-[6px] font-semibold pb-10">
           Welcome To My Page
         </div>
+      </div>
+      <div className="flex justify-center w-full">
+        <button className="flex justify-center items-center bg-white text-Text-title w-[184px] h-12 rounded-full border-4 border-button-primary">
+          About Me
+        </button>
       </div>
     </div>
   );

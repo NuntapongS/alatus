3c9207071abfa933ac3bbf7ca9bd440bb73d3b3d
NuntapongS <nuntapong@odds.team>
2023-04-01 17:48:18 +0700

    add react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
+import HomePage from "./pages/HomePage";
+
+const route = createBrowserRouter([
+  {
+    path: "/home",
+    element: <HomePage />,
+  },
+]);
 
 function App() {
-  return <></>;
+  return <RouterProvider router={route} />;
 }
 
 export default App;

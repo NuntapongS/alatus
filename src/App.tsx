f5054577bced39b9ed40eca9cd9236d0e2d8384c
NuntapongS <nuntapong@odds.team>
2023-04-01 20:01:47 +0700

    add path about me

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
+import AboutMe from "./pages/AboutMe";
 import HomePage from "./pages/HomePage";
 
 const route = createBrowserRouter([
   {
     path: "/home",
     element: <HomePage />,
+  },
+  {
+    path: "/about-me",
+    element: <AboutMe />,
   },
 ]);
 

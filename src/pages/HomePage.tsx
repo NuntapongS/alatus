313308b7c63ad0b2523c622dbf3ee394059eecc1
NuntapongS <nuntapong@odds.team>
2023-04-01 19:45:58 +0700

    Revert "add user navigate in about me button"
    This reverts commit e3419dc9523eb9048b1827a04070bd111e729630.

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,4 @@
-import { useNavigate } from "react-router-dom";
-
 const HomePage = () => {
-  const navigate = useNavigate();
   return (
     <div className="h-screen w-full bg-[url('https://i.ytimg.com/vi/9sNQFJAb3Ss/maxresdefault.jpg')] bg-cover">
       <div className="flex flex-col ">
@@ -16,7 +13,6 @@ const HomePage = () => {
         <button
           className="flex justify-center items-center bg-white text-Text-title w-[184px] h-12 rounded-full border-4 border-button-primary"
           data-testid="about-me-button"
-          onClick={() => navigate("/about-me")}
         >
           About Me
         </button>

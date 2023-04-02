4b8a190045dd852148d6d33162d1c558d44914b8
NuntapongS <nuntapong@odds.team>
2023-04-02 19:56:58 +0700

    add blibk style on name

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,8 +5,11 @@ const HomePage = () => {
   return (
     <div className="h-screen w-full bg-[url('https://i.ytimg.com/vi/9sNQFJAb3Ss/maxresdefault.jpg')] bg-cover">
       <div className="flex flex-col ">
-        <div className="flex flex-col justify-center items-center text-3xl text-Text-title tracking-[6px] font-semibold pt-[230px] pb-10">
-          Nuntapong (Pat) Siripanyawong |
+        <div className="flex flex-col justify-center items-center text-3xl text-Text-title tracking-[6px]  pt-[230px] pb-10">
+          <div className="font-semibold">
+            Nuntapong (Pat) Siripanyawong
+            <span className="blink font-bold">|</span>
+          </div>
         </div>
         <div className="flex justify-center text-xl text-Text-title tracking-[6px] font-semibold pb-10">
           Welcome To My Page

d51d6cae47014910ddd632dc5b8d2bea86523fc0
NuntapongS <nuntapong@odds.team>
2023-04-02 20:04:52 +0700

    add padding on button in page about me

--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -4,7 +4,7 @@ const AboutMe = () => {
   const navigate = useNavigate();
   return (
     <div className="h-screen w-full bg-[url('https://i.ytimg.com/vi/9sNQFJAb3Ss/maxresdefault.jpg')] bg-cover">
-      <div className="flex justify-between">
+      <div className="flex justify-between p-4">
         <button
           className="flex justify-center items-center bg-white text-Text-title w-[184px] h-12 rounded-full border-4 border-button-boarder"
           onClick={() => navigate("/home")}

07defda536410523cdfa39fef3df619876e9dc8b
NuntapongS <nuntapong@odds.team>
2023-04-02 19:49:18 +0700

    add back button

--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -1,5 +1,17 @@
+import { useNavigate } from "react-router-dom";
+
 const AboutMe = () => {
-  return <div>About Me</div>;
+  const navigate = useNavigate();
+  return (
+    <div className="h-screen w-full bg-[url('https://i.ytimg.com/vi/9sNQFJAb3Ss/maxresdefault.jpg')] bg-cover">
+      <button
+        className="flex justify-center items-center bg-white text-Text-title w-[184px] h-12 rounded-full border-4 border-button-primary"
+        onClick={() => navigate("/home")}
+      >
+        Back
+      </button>
+    </div>
+  );
 };
 
 export default AboutMe;

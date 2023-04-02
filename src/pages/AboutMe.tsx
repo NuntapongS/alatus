16636af6dfb24ad80465319ff5e9f3628e10998e
NuntapongS <nuntapong@odds.team>
2023-04-02 19:52:21 +0700

    add portfolio button

--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -4,12 +4,20 @@ const AboutMe = () => {
   const navigate = useNavigate();
   return (
     <div className="h-screen w-full bg-[url('https://i.ytimg.com/vi/9sNQFJAb3Ss/maxresdefault.jpg')] bg-cover">
-      <button
-        className="flex justify-center items-center bg-white text-Text-title w-[184px] h-12 rounded-full border-4 border-button-primary"
-        onClick={() => navigate("/home")}
-      >
-        Back
-      </button>
+      <div className="flex justify-between">
+        <button
+          className="flex justify-center items-center bg-white text-Text-title w-[184px] h-12 rounded-full border-4 border-button-primary"
+          onClick={() => navigate("/home")}
+        >
+          Back
+        </button>
+        <button
+          className="flex justify-center items-center bg-white text-Text-title w-[184px] h-12 rounded-full border-4 border-button-primary"
+          onClick={() => navigate("/home")}
+        >
+          Portfolio
+        </button>
+      </div>
     </div>
   );
 };

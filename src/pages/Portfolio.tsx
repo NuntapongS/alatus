fd1e75cf1ccc9ec34bad165b3c21dd6b0b77ddae
NuntapongS <nuntapong@odds.team>
2023-04-05 09:24:18 +0700

    add button in portfolio page

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -1,5 +1,24 @@
+import { useNavigate } from "react-router-dom";
+
 const Portfolio = () => {
-  return <></>;
+  const navigate = useNavigate();
+
+  const onBack = () => {
+    navigate(-1);
+  };
+
+  return (
+    <div className="h-screen w-full bg-[url('https://i.ytimg.com/vi/9sNQFJAb3Ss/maxresdefault.jpg')] bg-cover">
+      <div className="flex justify-between p-4">
+        <button
+          className="flex justify-center items-center bg-white text-text-back w-[184px] h-12 rounded-full border-4 border-boarder-back"
+          onClick={onBack}
+        >
+          Back
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Portfolio;

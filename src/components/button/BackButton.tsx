4e589be2cdd287baf428a97b4904ed5184c375ef
NuntapongS <nuntapong@odds.team>
2023-07-19 08:33:08 +0700

    get style for test

--- src/components/button/BackButton.tsx
+++ src/components/button/BackButton.tsx
@@ -4,13 +4,14 @@ type navigateButtonProps = {
 
 const NavigateBackButton = (props: navigateButtonProps) => {
   return (
-    <button
-      className="flex justify-center items-center bg-black hover:bg-border-next font-serif text-white w-[184px] h-12 rounded-full border-4 border-border-back"
-      onClick={props.onClick}
-    >
+    <button className={styleBackButton()} onClick={props.onClick}>
       Back
     </button>
   );
 };
 
 export default NavigateBackButton;
+
+const styleBackButton = () => {
+  return "flex justify-center items-center bg-black hover:bg-border-next font-serif text-white w-[184px] h-12 rounded-full border-4 border-border-back";
+};
--- src/components/button/NextButton.tsx
+++ src/components/button/NextButton.tsx
@@ -5,13 +5,14 @@ type nextButtonProps = {
 
 const NextButton = (props: nextButtonProps) => {
   return (
-    <button
-      className="flex justify-center items-center font-serif bg-black hover:bg-border-next text-white  w-[184px] h-12 rounded-full border-4 border-border-next"
-      onClick={props.onClick}
-    >
+    <button className={styleNextButton()} onClick={props.onClick}>
       {props.message}
     </button>
   );
 };
 
 export default NextButton;
+
+const styleNextButton = () => {
+  return "flex justify-center items-center font-serif bg-black hover:bg-border-next text-white  w-[184px] h-12 rounded-full border-4 border-border-next";
+};

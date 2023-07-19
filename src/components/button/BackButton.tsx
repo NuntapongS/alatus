7be09dfcb9dcfe02339daad1e62633ac5012ea3a
NuntapongS <nuntapong@odds.team>
2023-07-19 08:45:30 +0700

    export for test

--- src/components/button/BackButton.tsx
+++ src/components/button/BackButton.tsx
@@ -12,6 +12,6 @@ const NavigateBackButton = (props: navigateButtonProps) => {
 
 export default NavigateBackButton;
 
-const styleBackButton = () => {
+export const styleBackButton = () => {
   return "flex justify-center items-center bg-black hover:bg-border-next font-serif text-white w-[184px] h-12 rounded-full border-4 border-border-back";
 };

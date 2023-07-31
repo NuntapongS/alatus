8c4c8bddcdaa4fe2f7cc6a9f2ed3fba3e5ee5ea1
NuntapongS <nuntapong@odds.team>
2023-07-31 08:35:13 +0700

    add content in portfolio page

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -20,6 +20,23 @@ const Portfolio = () => {
           }}
         />
       </div>
+      <div className="flex min-h-max gap-5 text-white pt-40">
+        <div className="flex max-h-max w-full justify-center items-center">
+          <img
+            // src={profile}
+            className="flex border-4 border-border-primary rounded-full h-96 w-96 object-cover"
+            alt=""
+          />
+        </div>
+        <div className="w-full font-serif font-bold">
+          <div>
+            <div className="text-2xl text-text-title">Portfolio</div>
+            <div className="my-10">
+              <i className="fa-solid fa-file-lines fa-2xl"></i>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };

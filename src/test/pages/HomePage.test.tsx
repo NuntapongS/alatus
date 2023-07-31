7a3533512c9df49d63b518df7b0de459c49ebdb3
NuntapongS <nuntapong@odds.team>
2023-07-31 08:03:24 +0700

    add test profile page

--- src/test/pages/HomePage.test.tsx
+++ src/test/pages/HomePage.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import HomePage from "../../pages/HomePage";
 import { MemoryRouter } from "react-router-dom";
 
---
+++ src/test/pages/Profile.test.tsx
@@ -0,0 +1,25 @@
+import { render, screen } from "@testing-library/react";
+import Profile from "../../pages/Profile";
+import { MemoryRouter } from "react-router-dom";
+
+describe("Profile", () => {
+  test("user should see text hello world", () => {
+    render(
+      <MemoryRouter>
+        <Profile />
+      </MemoryRouter>
+    );
+    const textGreeting = screen.getByText(/Hello World/i);
+    expect(textGreeting).toBeInTheDocument();
+  });
+
+  test("user should see text welcome to my profile", () => {
+    render(
+      <MemoryRouter>
+        <Profile />
+      </MemoryRouter>
+    );
+    const textWelcome = screen.getByText(/Welcome To My Profile/);
+    expect(textWelcome).toBeInTheDocument();
+  });
+});
